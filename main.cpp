4b6d9a48a87d86934b33a57371bf7471a75cc9e2
NeoHugh <945214467@qq.com>
2020-06-26 05:12:37 +0800

    6.26update
    define static member variables(debug

--- main.cpp
+++ main.cpp
@@ -1,19 +1,34 @@
 #include"Catalog.h"
 #include"global.h"
+#include"IndexManager.h"
+#include"RecordManager.h"
 #include"Interpreter.h"
+#include"BufferManager.h"
+#include"Page.h"
 
 using namespace std;
 
+int BufferManager::lruCounter[CACHE_CAPACITY];
+Page BufferManager::cachePages[CACHE_CAPACITY];
+bool BufferManager::pined[CACHE_CAPACITY];
+bool BufferManager::isDirty[CACHE_CAPACITY];
+//需要在全局范围内重新定义一次
+
+Interpreter i;
+RecordManager record;
+IndexManager index;
+CatalogManager catalog;
+BufferManager buf;
+
 int main()
 {
-	Interpreter i;
+
+	bool execfile = false;
 	while (i.flag == true)
 	{
 		i.Query();
 		i.Choice();
 	}
-	
-
 	system("pause");
 	return 0;
 }
