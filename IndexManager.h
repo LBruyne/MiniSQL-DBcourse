81aca14e38e0c34482e8ae04a18c371782884629
NeoHugh <945214467@qq.com>
2020-07-03 17:23:31 +0800

    7.3
    新版B+树

--- IndexManager.h
+++ IndexManager.h
@@ -1,8 +1,8 @@
 /*
  * File name: IndexManager.h
  * Author: Xuanming, Liu
- * Latest revised: 2020.06.16 
- * Description: 
+ * Latest revised: 2020.06.16
+ * Description:
  * Header for IM module.
  * Function:
  * 1. Using B+ tree to realize indexing.
@@ -22,25 +22,25 @@ public:
 	std::string bptIntName;
 	std::string bptFloatName;
 	std::string bptStringName;
-	BPlusTree<int> *bpt_INT;
-	BPlusTree<float> *bpt_FLOAT;
-	BPlusTree<string> *bpt_STRING;
+	BPlusTree<int>* bpt_INT;
+	BPlusTree<float>* bpt_FLOAT;
+	BPlusTree<string>* bpt_STRING;
 
 public:
 	IndexManager();
-	bool CreateIndex(Table &table, Index &index);
-	bool CreateIndex(Table &table);
-	bool HasIndex(const std::string &TableName, std::string &AttribName);
-	bool DropIndex(const std::string &TableName, std::string &AttribName);
+	bool CreateIndex(Table& table, Index& index);
+	bool CreateIndex(Table& table);
+	bool HasIndex(const std::string& TableName, std::string& AttribName);
+	bool DropIndex(const std::string& TableName, std::string& AttribName);
 	void ResetBptInt(bool save);
 	void ResetBptFloat(bool save);
 	void ResetBptString(bool save);
 	void SaveIndex();
-	bool LoadIndex(std::string &TableName, std::string &AttribName, int type);
-	bool InsertItem(const std::string &value, Pointer pointer, int type);
-	bool DeleteItem(std::string &value, int type) ;
-	bool DeletePointers(std::vector<Pointer> &pointers, int type);
-	bool SelectItem(Condition &cond, int type, std::vector<Pointer> &pointers);
+	bool LoadIndex(std::string& TableName, std::string& AttribName, int type);
+	bool InsertItem(const std::string& value, Pointer pointer, int type);
+	bool DeleteItem(std::string& value, int type);
+	bool DeletePointers(std::vector<Pointer>& pointers, int type);
+	bool SelectItem(Condition& cond, int type, std::vector<Pointer>& pointers);
 	Pointer FindPointer(std::string& value, int type);
 	bool Find(std::string& value, int type);
 	//void printS();//test
